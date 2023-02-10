78c3785c6c9e117d322027900998cd776e57e5de
Georg Bremer <github@dschoordsch.de>
2023-02-10 11:49:51 +0100

    Try again

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import ReviewStats from './reviewStats'
 async function run(): Promise<void> {
   try {
     const slackWebhook = core.getInput('slack-webhook', {required: true})
-    core.info(`REPO: ${process.env.GITHUB_REPOSITORY}`)
     const repository =
       core.getInput('repository') || process.env.GITHUB_REPOSITORY!
+    core.info(`REPO: ${repository}`)
     const githubToken =
       core.getInput('github-token') || process.env.GITHUB_TOKEN!
     const timeDiff = core.getInput('time-diff') || '7d'
